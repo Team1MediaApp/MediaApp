04dc6fa557a90c87199b94165a51c519f3717558
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-26 09:55:36 +0900

    Config Build  : viewbinding 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
