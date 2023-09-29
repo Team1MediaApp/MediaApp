8bc67cd1555973000cfbd7b3c5d61873a2a6ead5
김진아 <c2010333@naver.com>
2023-09-29 21:00:38 +0900

    [Feat] 공유하기, 좋아요 버튼 구현 주석

--- app/src/main/java/com/example/mediaapp/DetailFragment.kt
+++ app/src/main/java/com/example/mediaapp/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mediaapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,7 +14,23 @@ class DetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // shareButton()
     }
+
+// 공유하기 버튼 기능 구현
+//    private fun shareButton(){
+//        val shareButton = binding.detailBtnShare
+//        shareButton.setOnClickListener {
+//            val shareIntent = Intent().apply() {
+//                action = Intent.ACTION_SEND
+//                putExtra(
+//                    Intent.EXTRA_TEXT, "youtube.com") <- 전달되고자 하는 URL부분
+//                type = "text/plain"
+//            }
+//            startActivity(Intent.createChooser(shareIntent, null))
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
