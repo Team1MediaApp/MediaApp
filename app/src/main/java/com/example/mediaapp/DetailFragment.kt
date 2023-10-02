2bb5a36b50357dab4b703fb038bcec59bc66c0e1
김진아 <c2010333@naver.com>
2023-10-02 09:46:12 +0900

    [Feat] HomeFragment에서 Item 클릭 시 DetailFragment로 정보 연결

--- app/src/main/java/com/example/mediaapp/DetailFragment.kt
+++ app/src/main/java/com/example/mediaapp/DetailFragment.kt
@@ -2,10 +2,15 @@ package com.example.mediaapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import coil.load
+import com.example.mediaapp.data.model.video.Item
 import com.example.mediaapp.databinding.DetailFragmentBinding
 
 class DetailFragment : Fragment() {
@@ -14,25 +19,23 @@ class DetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         // shareButton()
     }
 
-// 공유하기 버튼 기능 구현
-//    private fun shareButton(){
-//        val shareButton = binding.detailBtnShare
-//        shareButton.setOnClickListener {
-//            val shareIntent = Intent().apply() {
-//                action = Intent.ACTION_SEND
-//                putExtra(
-//                    Intent.EXTRA_TEXT, "youtube.com") <- 전달되고자 하는 URL부분
-//                type = "text/plain"
-//            }
-//            startActivity(Intent.createChooser(shareIntent, null))
-//        }
-//    }
+
+ //공유하기 버튼 기능 구현
+    private fun shareButton(){
+        val shareButton = binding.detailBtnShare
+        shareButton.setOnClickListener {
+            val shareIntent = Intent().apply() {
+                action = Intent.ACTION_SEND
+                putExtra(
+                    Intent.EXTRA_TEXT, "youtube.com") // <- 전달되고자 하는 URL부분
+                type = "text/plain"
+            }
+            startActivity(Intent.createChooser(shareIntent, null))
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,5 +43,19 @@ class DetailFragment : Fragment() {
     ): View {
         _binding = DetailFragmentBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("jina", "DetailFragment: arguments")
+        val item = arguments?.getSerializable("Video_data") as? Item
+        item?.let {
+            with(binding){
+                detailTxtChannel.text = it.snippet.channelTitle
+                detailTxtVideoTitle.text = it.snippet.title
+                detailImgThumnail.load(it.snippet.thumbnails.medium.url)
+                detailTxtVideoDetail.text = it.snippet.description
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mediaapp/HomeFragment.kt
+++ app/src/main/java/com/example/mediaapp/HomeFragment.kt
@@ -14,13 +14,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mediaapp.data.model.channel.ChItem
 import com.example.mediaapp.util.CategoryId
 import com.example.mediaapp.data.model.video.Item
-import com.example.mediaapp.databinding.DetailFragmentBinding
 import com.example.mediaapp.databinding.HomeFragmentBinding
+import com.example.mediaapp.ui.adapter.ChannelItemClick
 import com.example.mediaapp.ui.adapter.HomeCategoryRcvViewAdapter
 import com.example.mediaapp.ui.adapter.HomeChannelRcvAdapter
 import com.example.mediaapp.ui.adapter.HomeTrendingRcvAdapter
-import kotlinx.coroutines.Delay
-import okhttp3.internal.userAgent
+import com.example.mediaapp.ui.adapter.ItemClick
 
 
 class HomeFragment : Fragment() {
@@ -68,8 +67,8 @@ class HomeFragment : Fragment() {
             homeTrendingRcvViewAdapter.submitList(result)
         }
 
-        searchViewModel.searchResult.observe(viewLifecycleOwner) { response ->
-            val result: List<Item> = response.items
+        searchViewModel.searchResult.observe(viewLifecycleOwner){ response ->
+            val result:List<Item> = response.items
 
             homeCategoryRcvViewAdapter.submitList(result)
             Log.d("TAG", "submitList? : ${homeCategoryRcvViewAdapter.submitList(result)}")
@@ -94,32 +93,69 @@ class HomeFragment : Fragment() {
     }
 
 
-    private fun setupRecyclerView(){
+    private fun setupRecyclerView() {
         val trendingSnapHelper = LinearSnapHelper()
         trendingSnapHelper.attachToRecyclerView(binding.homeRcvTrendingList)
-        homeCategoryRcvViewAdapter = HomeCategoryRcvViewAdapter()
+        homeCategoryRcvViewAdapter = HomeCategoryRcvViewAdapter(object : ItemClick {
+            override fun onClick(item: Item) {
+                val detail = DetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putSerializable("Video_data", item)
+                    }
+                }
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame, detail)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        })
         binding.homeRcvCategoryList.apply {
             setHasFixedSize(true)
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = homeCategoryRcvViewAdapter
         }
-        homeTrendingRcvViewAdapter = HomeTrendingRcvAdapter()
+
+        homeTrendingRcvViewAdapter = HomeTrendingRcvAdapter(object : ItemClick {
+            override fun onClick(item: Item) {
+                val detail = DetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putSerializable("Video_data", item)
+                    }
+                }
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame, detail)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        })
+
         binding.homeRcvTrendingList.apply {
             setHasFixedSize(true)
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = homeTrendingRcvViewAdapter
             addOnScrollListener(trendingRcvListener)
         }
-        homeChannelRcvAdapter = HomeChannelRcvAdapter()
+
+        homeChannelRcvAdapter = HomeChannelRcvAdapter(object : ChannelItemClick {
+            override fun onClick(item: ChItem) {
+                Log.d("jina", "homeChannelAdapter: onClick $item")
+                val detail = DetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putSerializable("Video_data", item)
+                    }
+                }
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame, detail)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        })
+
         binding.homeRcvChannelList.apply {
             setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = homeChannelRcvAdapter
         }
-
-
     }
 
     private val trendingRcvListener = object : RecyclerView.OnScrollListener(){
@@ -162,5 +198,4 @@ class HomeFragment : Fragment() {
         _binding = null
         scrollHandler.removeCallbacksAndMessages(scrollRunnable)
     }
-
 }
