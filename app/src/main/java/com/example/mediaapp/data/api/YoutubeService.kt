d2b3a035ac4a3cc4399dfb3978dc8411dacc1eb0
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-03 01:36:57 +0900

    Feat : add serach vidoes&channel servie

--- app/src/main/java/com/example/mediaapp/data/api/YoutubeService.kt
+++ app/src/main/java/com/example/mediaapp/data/api/YoutubeService.kt
@@ -1,17 +1,34 @@
-package com.example.mediaapp
+package com.example.mediaapp.data.api
 
 import com.example.mediaapp.model.YoutubeSearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface YoutubeService {
-    @GET("search")
+    @GET("v3/search")
     suspend fun responseSearch(
         @Query("key") key: String,
         @Query("part") part: String,
         @Query("q") query: String,
-        @Query("maxResults") maxResults: Int,
-        @Query("regionCode") regionCode: String,
+        @Query("maxResults") maxResults: Int?,
+        @Query("regionCode") regionCode: String?,
+        @Query("type") type: String?,
+    ): YoutubeSearchResponse
+
+    @GET("v3/channels")
+    suspend fun responseChannels(
+        @Query("key") key: String,
+        @Query("part") part: String,
+//        @Query("categoryId") categoryId: String?,
+        @Query("forUsername") channelName: String?,
+//        @Query("hl") hl: String?,
+//        @Query("id") channelId: String?,
+//        @Query("managedByMe") managedByMe: Boolean? = null,
+//        @Query("maxResults") maxResults: Int?,
+//        @Query("mine") mine: Boolean? = null,
+//        @Query("mySubscribers") mySubscribers: Boolean? = null,
+//        @Query("onBehalfOfContentOwner") onBehalfOfContentOwner: String?,
+//        @Query("pageToken") pageToken: String?,
     ): YoutubeSearchResponse
 
 }
