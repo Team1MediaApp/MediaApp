7b2d48ecde3a14cdaa0cb4e6d97780b6992e5cad
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-03 00:38:04 +0900

    Feat : add retrofit client with okhttp

--- app/src/main/java/com/example/mediaapp/data/api/NetworkClient.kt
+++ app/src/main/java/com/example/mediaapp/data/api/NetworkClient.kt
@@ -1,21 +1,20 @@
-package com.example.mediaapp
+package com.example.mediaapp.data.api
 
+import com.example.mediaapp.BuildConfig
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import java.util.concurrent.TimeUnit
 
 object NetworkClient {
-    private const val YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/"
-
-    private fun createClient(): OkHttpClient {
-        val interceptor = HttpLoggingInterceptor()
-
-        if (BuildConfig.DEBUG) interceptor.level = HttpLoggingInterceptor.Level.BODY
-        else interceptor.level = HttpLoggingInterceptor.Level.NONE
-
-        return OkHttpClient.Builder()
+    private const val YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/"
+    private val interceptor = HttpLoggingInterceptor()
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
@@ -23,12 +22,10 @@ object NetworkClient {
             .build()
     }
 
-    fun getBuilder(): Retrofit = retrofitBuilder
-
     private val retrofitBuilder by lazy {
         Retrofit.Builder()
             .baseUrl(YOUTUBE_BASE_URL)
-            .client(createClient())
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
