cc7a69c0f9ddc7dc12b9d56ce559acf2f59f9f52
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-03 00:38:37 +0900

    Feat : add search repository

---
+++ app/src/main/java/com/example/mediaapp/data/api/SearchRepository.kt
@@ -0,0 +1,7 @@
+package com.example.mediaapp.data.api
+
+import com.example.mediaapp.model.YoutubeSearchResponse
+
+interface SearchRepository {
+    suspend fun getSearchImage(query: String, type: String): YoutubeSearchResponse
+}
