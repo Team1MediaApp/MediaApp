276b12c98fb8f66247e244d317d6e347912b82ca
김진아 <c2010333@naver.com>
2023-10-02 22:14:08 +0900

    [Feat] 뒤로가기 버튼 구현

--- app/src/main/java/com/example/mediaapp/DetailFragment.kt
+++ app/src/main/java/com/example/mediaapp/DetailFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toolbar
 import coil.load
 import com.example.mediaapp.data.model.video.Item
 import com.example.mediaapp.databinding.DetailFragmentBinding
@@ -17,9 +18,14 @@ class DetailFragment : Fragment() {
     private var _binding: DetailFragmentBinding? = null
     private val binding get() = _binding!!
 
+    private var url: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+// overridePendingTransition(R.anim.anim_left, R.anim.anim_left_exit)
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,16 +36,16 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
-    // 공유하기 버튼 구현 , 위에 shareButton() 포함
+    // 공유하기 버튼 구현, 위에 shareButton() 포함
     private fun shareButton(){
         binding.detailBtnShare.setOnClickListener {
             val shareIntent = Intent().apply() {
                 action = Intent.ACTION_SEND
                 putExtra(
-                    Intent.EXTRA_TEXT, "youtube.com") // <- 전달되고자 하는 URL부분
+                    Intent.EXTRA_TEXT, "https://www.youtube.com/watch?v=$url") // <- 공유할 URL 적기, 파이어베이스 - 다이나믹 링크
                 type = "text/plain"
             }
-            startActivity(Intent.createChooser(shareIntent, null))
+            startActivity(Intent.createChooser(shareIntent, "null"))
         }
     }
 
@@ -50,16 +56,22 @@ class DetailFragment : Fragment() {
 //        Log.d("jina", "DetailFragment: arguments")
         val item = arguments?.getSerializable("Video_data") as Item?
         item?.let {
-            with(binding){
+            with(binding) {
                 detailTxtChannel.text = it.snippet.channelTitle
                 detailTxtVideoTitle.text = it.snippet.title
                 detailImgThumnail.load(it.snippet.thumbnails.medium.url)
                 detailTxtVideoDetail.text = it.snippet.description
             }
+            url = it.id
+        }
+
+        // 뒤로가기 버튼 구현
+        binding.detailBtnBack.setOnClickListener {
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.detail_framelayout, HomeFragment()).commit()
         }
     }
 
-    // 메모리 누수 방지
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
