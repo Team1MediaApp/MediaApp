69fdc4768abff4ea3a6a94658e9f00c13357919b
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-04 02:41:04 +0900

    Feat : add channel list adapter

--- app/src/main/java/com/example/mediaapp/ui/adapter/SearchChannelListAdapter.kt
+++ app/src/main/java/com/example/mediaapp/ui/adapter/SearchChannelListAdapter.kt
@@ -1,29 +1,50 @@
 package com.example.mediaapp.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil.load
 import com.example.mediaapp.databinding.SearchChannelItemBinding
+import com.example.mediaapp.databinding.SearchVideoItemBinding
+import com.example.mediaapp.model.SearchChannelEntity
+import com.example.mediaapp.model.SearchVideoEntity
 import com.example.mediaapp.model.YoutubeSearchResponse
 
-class SearchChannelListAdapter() : RecyclerView.Adapter<ViewHolder>() {
+class SearchChannelListAdapter() :
+    RecyclerView.Adapter<SearchChannelListAdapter.ChannelViewHolder>() {
 
+    private val dataList: ArrayList<SearchChannelEntity> = arrayListOf()
+
+    fun refreshList() {
+        dataList.clear()
+        notifyDataSetChanged()
+    }
+
+    fun addDataList(items: ArrayList<SearchChannelEntity>) {
+        dataList.addAll(items)
+        notifyDataSetChanged()
+    }
 
     class ChannelViewHolder(private val binding: SearchChannelItemBinding) :
         ViewHolder(binding.root) {
-        fun bind() = with(binding) {
+        fun bind(item: SearchChannelEntity) = with(binding) {
+            itemImgVideo.load(item.channelImage)
+            itemTxtName.text = item.channel
+            itemTxtDescription.text = item.description
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChannelViewHolder {
+        return ChannelViewHolder(
+            SearchChannelItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = dataList.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ChannelViewHolder, position: Int) {
+        val item = dataList[position]
+        holder.bind(item)
     }
 }
