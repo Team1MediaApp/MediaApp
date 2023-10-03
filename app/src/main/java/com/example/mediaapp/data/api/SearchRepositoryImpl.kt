782315df9028987e11b0c8679c50ccd28c04fe98
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-04 02:44:45 +0900

    Feat : get data with pagetoken

--- app/src/main/java/com/example/mediaapp/data/api/SearchRepositoryImpl.kt
+++ app/src/main/java/com/example/mediaapp/data/api/SearchRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.mediaapp.data.api
 
 import com.example.mediaapp.model.YoutubeSearchResponse
 import com.example.mediaapp.util.Constants
+import retrofit2.http.Query
 
 class SearchRepositoryImpl() : SearchRepository {
     override suspend fun getSearchImage(
@@ -12,11 +13,25 @@ class SearchRepositoryImpl() : SearchRepository {
         NetworkClient.search.responseSearch(
             Constants.API_KEY,
             "snippet",
-            query,
             maxResult,
+            null,
+            query,
             "KR",
-            type
+            type,
         )
+
+    override suspend fun getSearchImageByPageToken(
+        nextPageToken: String,
+        maxResult: Int
+    ): YoutubeSearchResponse = NetworkClient.search.responseSearch(
+        Constants.API_KEY,
+        "snippet",
+        maxResult,
+        nextPageToken,
+        null,
+        null,
+        null,
+    )
 
     override suspend fun getSearchChannel(query: String): YoutubeSearchResponse =
         NetworkClient.search.responseChannels(Constants.API_KEY, "snippet", query)
