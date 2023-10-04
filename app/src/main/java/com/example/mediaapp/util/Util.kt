688bbab695ab3071e24a48237481033280b95849
김진아 <c2010333@naver.com>
2023-10-05 02:31:18 +0900

    [Fix] SharedPreferences 수정

--- app/src/main/java/com/example/mediaapp/util/Util.kt
+++ app/src/main/java/com/example/mediaapp/util/Util.kt
@@ -6,41 +6,26 @@ import com.example.mediaapp.data.model.video.Item
 import com.google.gson.GsonBuilder
 
 class Util {
-    /**
- * Shared Preferences에 아이템을 추가하는 함수.
- *
- * @param context 앱의 현재 컨텍스트
- * @param item 추가하려는 아이템
- */
+
 fun addPrefItem(context: Context, item: Item) {
-    val prefs = context.getSharedPreferences("pref", Activity.MODE_PRIVATE)
+    val prefs = context.getSharedPreferences("videoID", Activity.MODE_PRIVATE)
     val editor = prefs.edit()
     val gson = GsonBuilder().create()
+    editor.putString(item.snippet.title, gson.toJson(item))
     editor.putString(item.snippet.thumbnails.medium.url, gson.toJson(item))
         editor.apply()
 }
 
-    /**
-     * Shared Preferences에서 특정 URL을 키로 하는 아이템을 제거하는 함수.
-     *
-     * @param context 앱의 현재 컨텍스트
-     * @param url 제거하려는 아이템의 URL
-     */
-    fun deletePrefItem(context: Context, url: String) {
-        val prefs = context.getSharedPreferences("pref", Activity.MODE_PRIVATE)
+    fun deletePrefItem(context: Context, item: Item) {
+        val prefs = context.getSharedPreferences("videoID", Activity.MODE_PRIVATE)
         val editor = prefs.edit()
-        editor.remove(url)
+        editor.remove(item.snippet.title)
+        editor.remove(item.snippet.thumbnails.medium.url)
         editor.apply()
     }
 
-    /**
-     * Shared Preferences에서 모든 북마크 아이템을 가져오는 함수.
-     *
-     * @param context 앱의 현재 컨텍스트
-     * @return 북마크된 아이템의 ArrayList
-     */
     fun getPrefBookmarkItems(context: Context): ArrayList<Item> {
-        val prefs = context.getSharedPreferences("pref", Activity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences("videoID", Activity.MODE_PRIVATE)
         val allEntries: Map<String, *> = prefs.all
         val bookmarkItems = ArrayList<Item>()
         val gson = GsonBuilder().create()
@@ -49,5 +34,11 @@ fun addPrefItem(context: Context, item: Item) {
             bookmarkItems.add(item)
         }
         return bookmarkItems
+    }
+
+    // 좋아요 눌린 건지 아닌지 판단(기본적인 부분)
+    fun BookmarkCheck(context:Context, title:String): Boolean {
+        val prefs = context.getSharedPreferences("videoID", Activity.MODE_PRIVATE)
+        return prefs.contains(title)
     }
 }
