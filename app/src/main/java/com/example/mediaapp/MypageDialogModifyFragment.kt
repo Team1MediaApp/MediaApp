00e3bde4451d0a6dbfc27d7f38a567eef8ed2848
황진주 <ghkd1007@gmail.com>
2023-10-04 10:55:24 +0900

    [feat] 다이얼로그 // 저장 안되는거 버튼 잘 안움직이는거 수정함.

--- app/src/main/java/com/example/mediaapp/MypageDialogModifyFragment.kt
+++ app/src/main/java/com/example/mediaapp/MypageDialogModifyFragment.kt
@@ -1,43 +1,46 @@
 package com.example.mediaapp
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.mediaapp.data.MypageContext
 import com.example.mediaapp.databinding.MypageDialogModifyFragmentBinding
-
+import java.io.File
+import java.io.FileOutputStream
 
 class MypageDialogModifyFragment : DialogFragment() {
-//△이전에 show가 안되어서 문제였던 부분은 DialogFragment가 아닌 Fragment로 했기 때문에 기본적으로 다이얼로그에 있는
-// show가 없어서 생긴 문제였다고한다 다음에도 안되면 확인해보기
-
-    private var listener: OnDataModifiedListener? = null
-
-    fun setOnDataModifiedListener(listener: OnDataModifiedListener) {
-        this.listener = listener
-    }
+    private var _binding: MypageDialogModifyFragmentBinding? = null
+    private val binding get() = _binding!!
 
     interface OnDataModifiedListener {
         fun onDataModified()
     }
 
+    private var onDataModifiedListener: OnDataModifiedListener? = null
 
-    private var _binding: MypageDialogModifyFragmentBinding? = null
-    private val binding get() = _binding!!
+    fun setOnDataModifiedListener(listener: OnDataModifiedListener) {
+        onDataModifiedListener = listener
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
     }
+
+
+    private val REQUEST_CODE_PICK_BACKGROUND_IMAGE = 1000
+
+    private val REQUEST_CODE_PICK_PROFILE_IMAGE = 1001
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,74 +48,33 @@ class MypageDialogModifyFragment : DialogFragment() {
     ): View? {
         _binding = MypageDialogModifyFragmentBinding.inflate(inflater, container, false)
 
-//▽이름글자 수 제한을위해 몇글자 썼는지 표시하기
-        val mypage10limittNameReader =
-            binding.mypagemodifyEditName // 이름을 쓰는칸인데, 10자제한해야해서 몇자까지 썼는지 읽어오기 위해 불렀다.
-        val mypage10limitName = binding.mypagemodifyTxtNamecount // 10자 제한을 표시하는 txt이다.
-
-
-        mypage10limittNameReader.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                mypage10limitName.text = "${s?.length ?: 0}/10"
-            }
-        })
-
-        //▽상태글자 수 제한을위해 몇글자 썼는지 표시하기
-        val mypage20limitStatusReader =
-            binding.mypagemodifyEditStatus// 이름을 쓰는칸인데, 10자제한해야해서 몇자까지 썼는지 읽어오기 위해 불렀다.
-        val mypage20limitStatus = binding.mypagemodifyTxtStatuscount // 10자 제한을 표시하는 txt이다.
-
-
-        mypage20limitStatusReader.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                mypage20limitStatus.text = "${s?.length ?: 0}/20"
-            }
-        })
-
-
-        //내가 찜한 영상친구에게 공유하기 버튼 구현 ▼
-        val frindShareBtn = binding.mypagemodifyBtnShare
-
+        // 0.SharedPreferences 초기화(버튼값저장하고하는거 시작)
         val sharedPreferences =
             requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        //1.내가찜한영상친구에게공유하기
+        val markerShare = binding.mypagemodifyBtnShare
+       //2.쉐어프리퍼런스에서 기본값(고정)
+        val isMarkerShareOn = sharedPreferences.getBoolean("isMarkerShareOn", false)
+        // 버튼 상태 반전
+        var updatedState = isMarkerShareOn
 
-        var isFriendShareBtnOn = sharedPreferences.getBoolean("isFriendShareBtnOn", true)
-        setButtonState(frindShareBtn, isFriendShareBtnOn)
-        frindShareBtn.setOnClickListener {
+
+        //4.클릭리스너 설정
+        markerShare.setOnClickListener {
+            updatedState = !updatedState
             val editor = sharedPreferences.edit()
-            //버튼 상태 반전
-            isFriendShareBtnOn = !isFriendShareBtnOn
-            //현재 버튼상태 저장
-            editor.putBoolean("isFriendShareBtnOn", isFriendShareBtnOn)
-            editor.commit()
-
-            setButtonState(frindShareBtn, isFriendShareBtnOn)
+            // 현재 버튼 상태 저장
+            editor.putBoolean("isMarkerShareOn", updatedState)
+            // 변경사항 저장
+            editor.apply()
+            // 버튼 상태 변경
+            setButtonState(markerShare, updatedState)
         }
 
         return binding.root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
-
+    //5. 셋버튼스테이트 주기
     private fun setButtonState(button: AppCompatImageView, isOn: Boolean) {
         if (isOn) {
             // on 버튼일 때 이미지
@@ -123,5 +85,214 @@ class MypageDialogModifyFragment : DialogFragment() {
         }
     }
 
-}
 
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 이름 글자 수 제한을 위한 텍스트 변경 감지
+        binding.mypagemodifyEditName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                binding.mypagemodifyTxtNamecount.text = "${s?.length ?: 0}/10"
+            }
+        })
+
+        // 상태 글자 수 제한을 위한 텍스트 변경 감지
+        binding.mypagemodifyEditStatus.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                binding.mypagemodifyTxtStatuscount.text = "${s?.length ?: 0}/20"
+            }
+        })
+
+        // 백그라운드 이미지 선택 버튼 클릭 리스너
+        binding.mypagemodifyBtnBackgroundProfile.setOnClickListener {
+            openImagePicker(REQUEST_CODE_PICK_BACKGROUND_IMAGE)
+        }
+
+        // 프로필 이미지 선택 버튼 클릭 리스너
+        binding.mypagemodifyBtnProfile.setOnClickListener {
+            openImagePicker(REQUEST_CODE_PICK_PROFILE_IMAGE)
+        }
+
+        // 수정 시, 이전 내용 보여주기
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(MypageContext.PREF_MYPAGE, Context.MODE_PRIVATE)
+
+        // 이전 이름 보여주기
+        val previousName = sharedPreferences.getString(MypageContext.KEY_MYNAME, "")
+        binding.mypagemodifyEditName.setText(previousName)
+
+        // 이전 상태 메시지 보여주기
+        val previousMemo = sharedPreferences.getString(MypageContext.KEY_MYSTATUS, "")
+        binding.mypagemodifyEditStatus.setText(previousMemo)
+
+        // "수정 완료" 버튼 클릭 리스너
+        binding.mypagemodifyBtnSave.setOnClickListener {
+            saveUserInfoToPreferences()
+            dismiss()
+        }
+        loadToggleButton()
+        loadImageAndSaveToPreferences()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun openImagePicker(requestCode: Int) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, requestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_PICK_BACKGROUND_IMAGE && resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+
+            uri?.let {
+                val inputStream = requireActivity().contentResolver.openInputStream(uri)
+                val targetFile =
+                    File(requireActivity().filesDir, "backgroundImage.jpg") // 원하는 파일 이름으로 변경 가능
+                val outputStream = FileOutputStream(targetFile)
+
+                inputStream?.copyTo(outputStream)
+                inputStream?.close()
+                outputStream.close()
+
+                // 이미지 뷰에 선택된 이미지 설정
+                binding.mypagemodifyImgBackgroundprofile.setImageURI(Uri.fromFile(targetFile))
+
+                // 여기에 로깅 코드와 SharedPreferences 저장 코드를 추가
+                val newImagePath = targetFile.absolutePath
+                val sharedPreferences = requireActivity().getSharedPreferences(
+                    MypageContext.PREF_MYPAGE,
+                    Context.MODE_PRIVATE
+                )
+                val editor = sharedPreferences.edit()
+                editor.putString(MypageContext.KEY_MYIMAGE_BACK, newImagePath)
+                editor.apply()
+
+
+            }
+        } else if (requestCode == REQUEST_CODE_PICK_BACKGROUND_IMAGE) {
+            // 이미지 선택에 관한 기존 로직...
+            if (resultCode == Activity.RESULT_OK) {
+                binding.mypagemodifyImgBackgroundprofile.setImageURI(data?.data)  // 선택된 이미지를 img_myimage_modify에 설정
+            } else {
+                Toast.makeText(context, "이미지를 선택하지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // 프로필사진
+        if (requestCode == REQUEST_CODE_PICK_PROFILE_IMAGE && resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+
+            uri?.let {
+                val inputStream = requireActivity().contentResolver.openInputStream(uri)
+                val targetFile =
+                    File(requireActivity().filesDir, "profileImage.jpg") // 원하는 파일 이름으로 변경 가능
+                val outputStream = FileOutputStream(targetFile)
+
+                inputStream?.copyTo(outputStream)
+                inputStream?.close()
+                outputStream.close()
+
+                // 이미지 뷰에 선택된 이미지 설정
+                binding.mypagemodifyImgProfile.setImageURI(Uri.fromFile(targetFile))
+
+                // 여기에 로깅 코드와 SharedPreferences 저장 코드를 추가
+                val newImagePath = targetFile.absolutePath
+                val sharedPreferences = requireActivity().getSharedPreferences(
+                    MypageContext.PREF_MYPAGE,
+                    Context.MODE_PRIVATE
+                )
+                val editor = sharedPreferences.edit()
+                editor.putString(MypageContext.KEY_MYIMAGE, newImagePath)
+                editor.apply()
+
+                Log.d("Mypage", "Saved Image Path: $newImagePath")
+
+
+            }
+        } else if (requestCode == REQUEST_CODE_PICK_PROFILE_IMAGE) {
+            // 이미지 선택에 관한 기존 로직...
+            if (resultCode == Activity.RESULT_OK) {
+                binding.mypagemodifyImgProfile.setImageURI(data?.data)  // 선택된 이미지를 img_myimage_modify에 설정
+            } else {
+                Toast.makeText(context, "이미지를 선택하지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loadToggleButton(){
+        val sharedPreferences = requireActivity().getSharedPreferences(
+            "MyPrefs",
+            Context.MODE_PRIVATE
+        )
+        //저장된값 불러오기(버튼)
+        val toggleButton = sharedPreferences.getBoolean("isMarkerShareOn", false)
+        setButtonState(binding.mypagemodifyBtnShare, toggleButton)
+    }
+
+    private fun loadImageAndSaveToPreferences() {
+        val sharedPreferences = requireActivity().getSharedPreferences(
+            MypageContext.PREF_MYPAGE,
+            Context.MODE_PRIVATE
+        )
+
+
+
+        val profileImagePath = sharedPreferences.getString(MypageContext.KEY_MYIMAGE, null)
+        profileImagePath?.let {
+            val profileImageFile = File(it)
+            if (profileImageFile.exists()) {
+                Glide.with(requireActivity())
+                    .load(Uri.fromFile(profileImageFile))
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .into(binding.mypagemodifyImgProfile)
+
+            } else {
+                Log.e(
+                    "ImageLoad",
+                    "Profile image file does not exist at path: $profileImagePath"
+                )
+            }
+        }
+        val backgroundImagePath = sharedPreferences.getString(MypageContext.KEY_MYIMAGE_BACK, null)
+        backgroundImagePath?.let {
+            val profileImageFile = File(it)
+            if (profileImageFile.exists()) {
+                Glide.with(requireActivity())
+                    .load(Uri.fromFile(profileImageFile))
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .into(binding.mypagemodifyImgBackgroundprofile)
+
+            } else {
+                Log.e(
+                    "ImageLoad",
+                    "Profile image file does not exist at path: $backgroundImagePath"
+                )
+            }
+        }
+    }
+
+    private fun saveUserInfoToPreferences() {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(MypageContext.PREF_MYPAGE, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(MypageContext.KEY_MYNAME, binding.mypagemodifyEditName.text.toString())
+        editor.putString(MypageContext.KEY_MYSTATUS, binding.mypagemodifyEditStatus.text.toString())
+        editor.apply()
+        onDataModifiedListener?.onDataModified()
+    }
+}
--- app/src/main/java/com/example/mediaapp/MypageFragment.kt
+++ app/src/main/java/com/example/mediaapp/MypageFragment.kt
@@ -1,9 +1,12 @@
 package com.example.mediaapp
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,22 +19,9 @@ import com.example.mediaapp.data.MypageContext
 import com.example.mediaapp.databinding.MypageFragmentBinding
 import java.io.File
 
-
 class MypageFragment : Fragment(), MypageDialogModifyFragment.OnDataModifiedListener {
     private var _binding: MypageFragmentBinding? = null
     private val binding get() = _binding!!
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
-
-    // 멤버 변수 : sharedPreferences와 isBookmarkChannalOn 선언
-    private lateinit var sharedPreferences: SharedPreferences
-    private var isBookmarkChannalOn: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,56 +29,111 @@ class MypageFragment : Fragment(), MypageDialogModifyFragment.OnDataModifiedList
     ): View? {
         _binding = MypageFragmentBinding.inflate(inflater, container, false)
 
+        // SharedPreferences 초기화
+        val sharedPreferences =
+            requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
         //내가찜한채널 버튼(이미지뷰),수정버튼 변수설정
         val bookmarkChannalBtn = binding.mypageBtnBookmarkChannal
+
         // SharedPreferences에서 버튼 상태 불러오기, 기본값은 찜한채널 알림설정 on
-        val sharedPreferences =
-            requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        // 로드 기본값 ON
-        var isBookmarkChannalOn = sharedPreferences.getBoolean("isBookmarkChannalOn", true)
-        // 버튼 초기 상태 설정
-        setButtonState(bookmarkChannalBtn, isBookmarkChannalOn)
+        //반전하는 변수는 밖으로 빼줄것
+        val isBookmarkChannalOn = sharedPreferences.getBoolean("isBookmarkChannalOn", false)
+        var updatedState = isBookmarkChannalOn
+
+
+
         bookmarkChannalBtn.setOnClickListener {
-            val editor = sharedPreferences.edit()
             // 버튼 상태 반전
-            isBookmarkChannalOn = !isBookmarkChannalOn
-            // 현재 버튼 저장
-            editor.putBoolean("isBookmarkChannalOn", isBookmarkChannalOn)
-            //저장이 잘 안될때는 apply()대신 commit()을 써보라고 함.(에뮬 종료후 다시켰을때)
-            editor.commit()
+            updatedState = !updatedState
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("isBookmarkChannalOn", updatedState)
+            // 변경사항 저장
+            editor.apply()
             // 버튼 상태 변경
-            setButtonState(bookmarkChannalBtn, isBookmarkChannalOn)
+            setButtonState(bookmarkChannalBtn, updatedState)
         }
-        return binding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val binding = MypageFragmentBinding.bind(view)
-
-        //저장된 이름 및 이미지 불러오는거
-        updateData()
-
-
-//        내 페이지 수정버튼(계속문제가 발생한 부분, show를 다이얼로그쪽에 선언해줬는데 이상시 확인필요)
+        //수정 버튼 클릭 리스너 설정
         binding.mypageBtnModifyProfile.setOnClickListener {
             val mypageDialog = MypageDialogModifyFragment()
             mypageDialog.setOnDataModifiedListener(this)
-            Log.d("aaaa","bbbb")
             mypageDialog.show(parentFragmentManager, "MypageDialogModifyFragment")
         }
-
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    override fun onResume() {
-        super.onResume()
         updateData()
+        loadToggleButton()
+
+        return binding.root
+    }
+
+
+    override fun onDataModified() {
+        updateData()  // 데이터 수정 시에도 동일한 데이터 갱신 메소드 호출
+    }
+
+    private fun updateData() {
+        // SharedPreferences에서 정보 가져오기
+        val sharedPreferences = requireActivity().getSharedPreferences(
+            MypageContext.PREF_MYPAGE,
+            Context.MODE_PRIVATE
+        )
+
+        // 이름 가져와서 설정하기
+        binding.mypageTxtName.text =
+            sharedPreferences.getString(
+                MypageContext.KEY_MYNAME,
+                binding.mypageTxtName.text.toString()
+            )
+
+        // 상태메세지 가져와서 설정하기
+        binding.mypageTxtStatusMessage.text =
+            sharedPreferences.getString(
+                MypageContext.KEY_MYSTATUS,
+                binding.mypageTxtStatusMessage.text.toString()
+            )
+
+        val backgroundImagePath = sharedPreferences.getString(MypageContext.KEY_MYIMAGE_BACK, null)
+        backgroundImagePath?.let {
+            val imageFile = File(it)
+            if (imageFile.exists()) {
+                Glide.with(requireActivity())
+                    .load(Uri.fromFile(imageFile))
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .into(binding.mypageImgBackgroundprofile)
+            }
+        }
+
+        // 프로필 이미지 경로 가져와서 Glide를 사용하여 이미지 로드하기
+        val profileImagePath = sharedPreferences.getString(MypageContext.KEY_MYIMAGE, null)
+        profileImagePath?.let {
+            val profileImageFile = File(it)
+            if (profileImageFile.exists()) {
+                Glide.with(requireActivity())
+                    .load(Uri.fromFile(profileImageFile))
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .into(binding.mypageImgProfile)
+            } else {
+                Log.e(
+                    "ImageLoad",
+                    "Profile image file does not exist at path: $profileImagePath"
+                )
+            }
+        }
+
+        //아래 추가부문
+        val profileImageFile = File(profileImagePath)
+        if (profileImageFile.exists()) {
+            Glide.with(requireActivity())
+                .load(Uri.fromFile(profileImageFile))
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .into(binding.mypageImgProfile)
+        } else {
+            Log.e("ImageLoad", "Profile image file does not exist at path: $profileImagePath")
+        }
+
     }
 
 
@@ -102,48 +147,15 @@ class MypageFragment : Fragment(), MypageDialogModifyFragment.OnDataModifiedList
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-        val editor = sharedPreferences.edit()
-        editor.putBoolean("isBookmarkChannalOn", isBookmarkChannalOn)
-        editor.commit()
+    private fun loadToggleButton() {
+        val sharedPreferences = requireActivity().getSharedPreferences(
+            "MyPrefs",
+            Context.MODE_PRIVATE
+        )
+        //저장된값 불러오기(버튼)
+        val toggleButton = sharedPreferences.getBoolean("isMarkerShareOn", false)
+        setButtonState(binding.mypageBtnBookmarkChannal, toggleButton)
     }
 
-    // ▽ 수정부분
-    override fun onDataModified() {
-        updateData()  // 데이터 수정 시에도 동일한 데이터 갱신 메소드 호출
-    }
 
-
-    private fun updateData() {
-        // SharedPreferences에서 정보 가져오기
-        val sharedPreferences =
-            requireActivity().getSharedPreferences(MypageContext.PREF_MYPAGE, Context.MODE_PRIVATE)
-
-        // 이름 가져와서 설정하기
-        binding.mypageTxtName.text =
-            sharedPreferences.getString(MypageContext.KEY_MYNAME, binding.mypageTxtName.text.toString())
-
-
-
-        // 상태메세지 가져와서 설정하기
-        binding.mypageTxtStatusMessage.text =
-            sharedPreferences.getString(MypageContext.KEY_MYSTATUS, binding.mypageTxtStatusMessage.text.toString())
-
-
-        // 이미지 경로 가져와서 Glide를 사용하여 이미지 로드하기
-        val savedImagePath = sharedPreferences.getString(MypageContext.KEY_MYIMAGE, null)
-        savedImagePath?.let {
-            val imageFile = File(it)
-            if (imageFile.exists()) {
-                Glide.with(requireActivity())
-                    .load(Uri.fromFile(imageFile))
-                    .diskCacheStrategy(DiskCacheStrategy.NONE)
-                    .skipMemoryCache(true)
-                    .into(binding.mypageImgProfile)
-            }
-        }
-    }
 }
-
-
